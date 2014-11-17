c0daf3b398bfff19f1a0d0c0c747ec9216869450
Frederic Marec <29000444@e003m08.istic.univ-rennes1.fr>
2014-11-17 12:15:20 +0100

    First commit

---
+++ exemple-main.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+import A
+import B
+
+
+def main():
+    
+    whichLib=A
+    whichLib.bonjour()
+    whichLib=B
+    whichLib.bonjour()
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+        
+        
+        
