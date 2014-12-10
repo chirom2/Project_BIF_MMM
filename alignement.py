fcf4038f87c6f3f128fa434be841e8652e099a5c
romain <romain@romain-X75VD.(none)>
2014-12-10 01:27:22 +0100

    Alignement avec Hashtable (Manque alignement pour les k carac du kmer)

--- alignement.py
+++ alignement.py
@@ -18,7 +18,6 @@ def alignementSA(read, dmax, sizeK,s, sa):
 	i=0
 	kRead = read[i:sizeK]#the sizeK first char ex:acgtgtttcca
 	pos = suffixeArray.GET_ALLpos(kRead,s,sa)
-	#suffixeArray.printPos(pos)
 	size = ((len(read))-1)
 	while (i <= len(pos)-1):
 		sRead = ""
@@ -41,42 +40,56 @@ def alignementSA(read, dmax, sizeK,s, sa):
 		print(sRead)
 		print(sMatch)
 		print(sGen)	
-
+#
+#Alignement for the hashtab
+#
 def alignementHT(read, dmax, sizeK,s, T):
 	ht = hashtab
 	kRead = ""
-	pos = []
 	sRead = ""
 	sMatch = ""
 	sGen = ""
+	numAlign = 0
 	i=0
 	sSize=len(s)
+	pos = []
 	kRead = read[0:sizeK]#the sizeK first char ex:acgtgtttcca
-	print("**********************************************************************")
-	print(kRead)
-	print("**********************************************************************")
-
-	pos = ht.chain_query(T,kRead,sSize)
-	print(pos)
+	pos = ht.chain_query(T,kRead,sSize)#return a list of list
 	size = ((len(read))-1)
-	while (i <= len(pos)-1):
-		sRead = ""
-		sMatch = ""
-		sGen = ""
-		j=0
-		d=0 #counter of Gap
-		x=pos[i]
-		while(j < (size) and d < dmax):#skip the car '$' 
-			sRead += read[j]
-			sGen += s[x]	
-			if(read[j] != s[x]):				
-				sMatch += ':'#misMatch				
-				d += 1
-			else:
-				sMatch += '|'#Match
-			j+=1
-			x+=1
-		i+=1
-		print(sRead)
-		print(sMatch)
-		print(sGen)	
+
+	if (pos != None and pos != []):
+		sizePos = len(pos[0])
+		#print "sizePOs ", sizePos
+		while(i < sizePos):	
+			sRead = ""
+			sMatch = ""
+			sGen = ""
+			j=0
+			d=0 #counter of Gap
+			x=pos[0][i]#the position ofthe kmer
+			#print "len pos", len(pos)
+			while(j < (size) and d < dmax):#skip the car '$' 
+				sRead += read[j]
+				sGen += s[x]	
+				if(read[j] != s[x]):				
+					sMatch += ':'#misMatch				
+					d += 1
+				else:
+					sMatch += '|'#Match
+				j+=1
+				x+=1
+				
+			print " alignement>>",numAlign	
+			print " #pos",pos[0][i]
+			print " #strand=+",-1#TODO check this parameter
+			print " #d=",d	
+			print " ",sRead
+			print " ",sMatch
+			print " ",sGen
+			print("\n") 
+			i+=1	
+			numAlign += 1
+		
+def printPos(listPos):
+	for i in range(0, len(listPos)):
+		print listPos[i]		
--- hashtab.py
+++ hashtab.py
@@ -3,7 +3,70 @@ import tools_MMM
 
 #s = sequence
 #k = size of Kmer
+#T[ [i, kmer], [i, kmer]... ]
 def chain_index(s,k):
+	m = len(s)
+	found=0
+	tools = tools_MMM
+	T=[]
+	kmer=None
+	Ksize=k
+	st=""
+	for i in range(0,m-1):
+		T[i]=T.append(None)		#Init of an empty Table
+	for i in range(0,(len(s)-k)):
+		st = s[i:(i+Ksize)]
+		pos=[i]
+		objectkmer=[pos,st]#POS or I [ [list, of ,postion] , "kmer"] => element of the hashtable
+		adr = tools.basicHash(objectkmer[1],m)
+		if (T[adr] == None):
+			T[adr] = [objectkmer]
+		else: #the kmer is not present
+			for j in range(0,len(T[adr])):				
+				k = T[adr][j]
+				#print "k0 ",k[0],"k[1] ",k[1]," kmer[0]",objectkmer[0]," objectkmer[1]", objectkmer[1]
+				if (objectkmer[1] == k[1]):#append
+					if k[0].count(objectkmer[0]) ==0:#the position is not present we add it
+						k[0].append(objectkmer[0][0])#add just a postion
+						#print "after append", k[0]
+					found=1
+				if (found==0):
+					T[adr].append(objectkmer)
+						
+	return T
+	
+#found an occcur of w in an hashtab 
+#Param: T = HashTab, w = string to found, sSize = size of the indexed text 
+#Return: position's list of w, None if w is not present in the hashtab
+#hash looks like T[ ]
+def chain_query(T,w,sSize):
+	tools = tools_MMM
+	adr= tools.basicHash(w,sSize)
+	listPos = []
+	if(T[adr] != None):		
+		for j in range(0,len(T[adr])):
+			if (T[adr][j][1]==w):
+				listPos.append( T[adr][j][0])
+		printPos(listPos)		
+		return listPos		
+	return None			
+
+
+	
+def printPos(listPos):
+	for i in range(0, len(listPos)):
+		print listPos[i]
+
+
+
+
+
+
+####################BACK UP#######################
+#s = sequence
+#k = size of Kmer
+#T[ [i, kmer], [i, kmer]... ]
+def chain_index222222(s,k):
 	m = len(s)
 	found=0
 	tools = tools_MMM
@@ -16,26 +79,23 @@ def chain_index(s,k):
 	for i in range(0,(len(s)-k)):
 		st = s[i:(i+Ksize)]
 		pos=i
-		kmer=[i,st]
+		kmer=[pos,st]#
 		adr = tools.basicHash(kmer[1],m)
 		if (T[adr] == None):
 			T[adr] = [kmer]
-		else: #(found = 0)
+		else: #the kmer is not present
 			for j in range(0,len(T[adr])):
+				
 				k = T[adr][j]
-				if (kmer[1] == k[1]):
+				#print "kmer[0] ",kmer[0]
+				print "k0 ",k[0],"k[1] ",k[1]," kmer[0]",kmer[0]," kmer[1]", kmer[1]
+				if (kmer[1] == k[1]):#append
+										
 					k[0] = (kmer[0])
 					found=1
 				if (found==0):
-					T[adr].append(kmer)
+					T[adr].append(kmer)			
 	return T
+	
 
-def chain_query(T,w,sSize):
-	print(T)
-	tools = tools_MMM
-	adr= tools.basicHash(w,sSize)
-	print(adr)
-	for j in range(0,len(T[adr])):
-		if (T[adr][j][1]==w):
-			return T[adr][j][0]
-	return -1
+
